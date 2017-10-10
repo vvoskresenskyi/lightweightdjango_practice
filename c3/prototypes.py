f8025d1be334784a4bd73a6557e5bf7042ff5e64
Vadym Voskresenskyi <vadim.voskresenskiy@nixsolutions.com>
2017-10-10 19:49:06 +0300

    C3 Continue to working

--- c3/prototypes.py
+++ c3/prototypes.py
@@ -1,6 +1,10 @@
+import os
 import sys
 
 from django.conf import settings
+
+
+BASE_DIR = os.path.dirname(__file__)
 
 
 settings.configure(
@@ -11,13 +15,29 @@ settings.configure(
 	INSTALLED_APPS=(
 		'django.contrib.staticfiles',
 		#'django.contrib.webdesign',
-		'sitebuilder'.
+		'sitebuilder',
+	),
+	TEMPLATES = (
+	    {
+	        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+	        'DIRS': [
+	            os.path.join(BASE_DIR, 'templates'),
+	        ],
+	        'APP_DIRS': True,
+	        'OPTIONS': {
+	            'context_processors': [],
+	        },
+	    },
+	),
+	STATICFILES_DIRS=(
+		os.path.join(BASE_DIR, 'static'),
 	),
 	STATIC_URL='/static/',
+	SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
 )
 
 
 if __name__ == "__main__":
 	from django.core.management import execute_from_command_line
 
-	execute_from_command_line
+	execute_from_command_line(sys.argv)
